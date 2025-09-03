926fadaf9c64f2f55d771c92a4578154a62bde36
Ryan Truong <100522370+Ryan-Tru@users.noreply.github.com>
2025-09-03 15:11:24 +1000

    commit for stage 1

--- brainstorming.md
+++ brainstorming.md
@@ -4,6 +4,69 @@ This file is used to document your thoughts, approaches and research conducted a
 
 ## Firmware
 
+#### TASK 1
+I had some trouble setting/building libraries.
+like not finding Boost package for cmake.
+
+I figured that installing it into the docker image using apt-get PACKAGE would
+fix the issue, as it was missing dependencies. I googled the error message and
+found some StackOverflow posts explaining some fixes. I first thought that it
+was not finding some packages as it was named wrong (Boost_INCLUDE_DIR instead
+of Boost_INCLUDEDIR), but that was not the issue. so I just ended up installing
+it in the docker image and it worked :).
+
+I also had some troubles getting CMAKE started, as I was confused whether to
+just work on the one in `solutions/CMakeLists` or the `firmware/CMakeLists`, but I
+decided to use both, as `dbcppp` lives in the firmware dir. I just linked it
+inside the `solutions/CMakeLists`. 
+
+[I used this cmake tutorial](https://cmake.org/cmake/help/latest/guide/tutorial/Adding%20a%20Library.html)
+
+As for storing can frames I am using unordered maps like the dbcppp example. 
+
+Currently I am having trouble with storing them, as doing 
+`std::unique_ptr<dbcppp::INetwork> net;` creates a unique ptr (I am trying to
+populate multiple maps for each DBC file from a function.) meaning that it gets
+destroyed after I return from the function.
+
+Deepseek recommends that I return net and the message map as a pair using
+`std::pair`, so I can transfer the ownership of net to the caller using
+std::move. 
+
+```
+    std::string payload{};
+    if (idAndPayloadVec.size() > 1) {
+         payload = idAndPayloadVec[1];
+```
+
+Before this piece of code, I was just trying to get the payload without
+checking that it existed, causing seg faults.
+
+```
+    auto findMsg = messageMap.find(id);
+
+    if (findMsg != messageMap.end()) {
+        const dbcppp::IMessage* msg = findMsg->second;
+```
+
+same for this piece of code directly below that. I was trying to access some
+ids that did not exist in the map. leading me to find out about the find method
+in unordered_maps. found here [unordered map docs](https://en.cppreference.com/w/cpp/container/unordered_map.html)
+
+Other than that I just followed along with what the dbcppp example did. (had to
+do some digging aroung in the .h files to find methods and whatnot.)
+
+I had some trouble formatting the rawtophys value as well, but I searched
+through SO and found these:
+
+[Double formatting 1](https://stackoverflow.com/questions/19135953/formatting-a-double-value-to-1-decimal-place) 
+
+and 
+
+[Double formatting 2](https://stackoverflow.com/questions/73752186/how-do-you-use-setprecision-when-declaring-a-double-variable-in-c)
+
+I also decided to use docker cp to get the output file, as seemed more simple.
+
 ## Spyder
 
 ## Cloud
--- firmware/solution/main.cpp
+++ firmware/solution/main.cpp
@@ -1,8 +1,167 @@
-
 #include <iostream>
+#include <fstream>
+#include <memory>
+#include <unordered_map>
+#include <string>
+#include <utility>
+#include <sstream>
+#include <vector>
 
-int main()
-{   
-    std::cout << "Hello World!" << std::endl;
+#include "dbcppp/CApi.h"
+#include "dbcppp/Network.h"
+
+std::pair<
+    std::unique_ptr<dbcppp::INetwork>,
+    std::unordered_map<uint64_t, const dbcppp::IMessage *>
+> loadCanFrames(
+        const std::string &path
+);
+
+void parseData(
+        std::unordered_map<uint64_t, const dbcppp::IMessage *> &messageMap,
+        std::vector<std::string> idAndPayloadVec,
+        const std::string &timeStamp,
+        std::ofstream &outputFile
+);
+
+uint8_t convertToHex(char c);
+
+int main() {
+    // Load can frames
+    auto [controlNet, controlMessages] =
+        loadCanFrames("/app/dbc-files/ControlBus.dbc");
+    auto [sensorNet, sensorMessages] =
+        loadCanFrames("/app/dbc-files/SensorBus.dbc");
+    auto [tracktiveNet, tracktiveMessages] =
+        loadCanFrames("/app/dbc-files/TractiveBus.dbc");
+
+    std::ifstream logFile("/app/dump.log");
+
+    std::ofstream outputFile("/app/output.txt");
+
+    if (!logFile) {
+        std::cout << "could not open log file\n";
+        exit(1);
+    } else if (!outputFile) {
+        std::cout << "could not open output file for writing\n";
+        exit(1);
+    }
+
+    // can0 ControlBus.dbc
+    // can1 SensorBus.dbc
+    // can2 TracktiveBus.dbc
+    std::string line{};
+    std::string timeStamp{};
+    std::string interface{};
+    std::string idAndPayloadStr{};
+
+    while (std::getline(logFile, line)) {
+        std::istringstream strStream(line);
+        strStream >> timeStamp >> interface >> idAndPayloadStr;
+        std::vector<std::string> idAndPayloadVec;
+        std::string token;
+
+        std::istringstream idAndPayloadStream(idAndPayloadStr);
+        while (std::getline(idAndPayloadStream, token, '#')) {
+            idAndPayloadVec.push_back(token);
+        }
+
+        if (interface == "can0") {
+            parseData(controlMessages, idAndPayloadVec, timeStamp, outputFile);
+        } else if (interface == "can1") {
+            parseData(sensorMessages, idAndPayloadVec, timeStamp, outputFile);
+        } else if (interface == "can2") {
+            parseData(tracktiveMessages, idAndPayloadVec, timeStamp, outputFile);
+        }
+        std::cout << std::flush;
+    }
+
+    logFile.close();
+    outputFile.close();
+
     return 0;
+}
+
+// This function loads the can frame structure for the specified DBC file,
+// using an unordered map to store it. I basically just ripped this code
+// straight out of the DBCPPP example.
+std::pair<
+    std::unique_ptr<dbcppp::INetwork>,
+    std::unordered_map<uint64_t, const dbcppp::IMessage *>
+> loadCanFrames(
+        const std::string &path
+) {
+    std::unique_ptr<dbcppp::INetwork> net;
+    {
+        std::ifstream idbc(path);
+        net = dbcppp::INetwork::LoadDBCFromIs(idbc);
+    }
+
+    if (net.get() == nullptr) {
+        std::cerr << "failed to parse!\n";
+        exit(-1);
+    }
+
+    std::unordered_map<uint64_t, const dbcppp::IMessage *> messages;
+    for (const dbcppp::IMessage& msg : net->Messages()) {
+        messages.insert(std::make_pair(msg.Id(), &msg));
+    }
+
+    return {std::move(net), std::move(messages)};
+}
+
+void parseData(
+        std::unordered_map<uint64_t, const dbcppp::IMessage *> &messageMap,
+        std::vector<std::string> idAndPayloadVec,
+        const std::string &timeStamp,
+        std::ofstream &outputFile
+) {
+    uint64_t id{};
+    id = std::stoi(idAndPayloadVec[0], nullptr, 16);
+
+    uint8_t data[8]{};
+
+    std::string payload{};
+    if (idAndPayloadVec.size() > 1) {
+         payload = idAndPayloadVec[1];
+        for (int i = 0; i < payload.length(); i += 2) {
+            uint8_t byte = (convertToHex(payload[i]) << 4)
+                | (convertToHex(payload[i + 1]));
+
+            data[i / 2] = byte;
+        }
+    }
+
+    auto findMsg = messageMap.find(id);
+
+    if (findMsg != messageMap.end()) {
+        const dbcppp::IMessage* msg = findMsg->second;
+        std::string extractedData{};
+        std::ostringstream valStream{};
+
+        for (const dbcppp::ISignal& sig : msg->Signals()) {
+            //get formatted val
+            valStream << std::fixed << std::setprecision(1)
+                << sig.RawToPhys(sig.Decode(data));
+
+            extractedData = timeStamp + ": "
+                + sig.Name() + ": "
+                + valStream.str() + "\n";
+
+            outputFile << extractedData;
+
+            valStream.str("");
+            valStream.clear();
+            // std::cout << timeStamp << ": " << sig.Name() << ": "
+            //     << sig.RawToPhys(sig.Decode(data)) << "\n";
+        }
+    }
+}
+
+// converts the char to it's specified hex val using the ascii representation.
+uint8_t convertToHex(char c) {
+    if (c >= '0' && c <= '9') return c - '0';
+    if (c >= 'A' && c <= 'F') return 10 + (c - 'A');
+    if (c >= 'a' && c <= 'f') return 10 + (c - 'a');
+    throw std::invalid_argument("Invalid hex character");
 }
